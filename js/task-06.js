eb3fc30394b7235bf2bb45eb0777d1c84093d712
Dmitrij <dmitrij.prokhorenko@gmail.com>
2019-10-25 10:44:53 +0300

    all done module 1

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,17 @@
 'use strict';
 let input;
 let total = 0;
+input = prompt();
 
-while (prompt() !== null) {
-  total += Number.parseFloat(prompt());
+while (input !== null && input !== NaN) {
+  while (isNaN(input)) {
+    alert('Введено не число! Попоробуйте ещё');
+    input = 0;
+  }
+  input = Number.parseFloat(input);
+  total += input;
   console.log(total);
+  input = prompt();
 }
+
 alert(`Сумма = ${total}`);
